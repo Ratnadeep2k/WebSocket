71c9edb4fed91f361a6500f73d70b8de2b78200c
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-02-19 01:05:16 +0530

    refactor WebSocket message handling to respond with 'pong' on 'ping' message

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { WebSocket } from "ws";
 const wss = new WebSocket.Server({ port: 8080 });
 wss.on("connection",function(socket){
-    console.log("New connection");
-    setInterval(() => {
-        socket.send("Hello Hello");
-    },500)
-    socket.on("message",(e)=>{
-        console.log(e.toString());
-    })
+    console.log("New connection connected");
+    
+    socket.on("message", (message) => {
+        console.log(message.toString()==="ping");
+        if (message.toString() === "ping") {
+            socket.send("pong");
+        }
+    }
+    );
 })
