f740fc3adfab833c5f90fb87c583f1c6a09c1bd6
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-02-19 00:19:38 +0530

    add WebSocket server implementation and update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+import { WebSocket } from "ws";
+const wss = new WebSocket.Server({ port: 8080 });
+wss.on("connection",function(socket){
+    console.log("New connection");
+    setInterval(() => {
+        socket.send("Hello Hello");
+    },500)
+    socket.on("message",(e)=>{
+        console.log(e.toString());
+    })
+})
