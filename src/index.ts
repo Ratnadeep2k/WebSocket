a20dbb307d2e472fecc205fb07ea9eee8f782bfa
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-02-19 01:09:21 +0530

    remove unnecessary whitespace in WebSocket connection handler

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { WebSocket } from "ws";
 const wss = new WebSocket.Server({ port: 8080 });
 wss.on("connection",function(socket){
     console.log("New connection connected");
-    
     socket.on("message", (message) => {
         console.log(message.toString()==="ping");
         if (message.toString() === "ping") {
